e7ea5c479b9b32bf32d03e02babb550a949ab12e
FelipeStella <felipedasilva.stella@gmail.com>
2021-10-07 00:05:11 -0300

    example-life-cycle

---
+++ React/lifecycle/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import Twitter from './Twitter'
+
+class App extends Component {
+
+  state = {
+    loading: false,
+    actived: true
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loading: true
+      })
+    }, 3000)
+  }
+
+  onRemove = () => {
+    this.setState({
+      actived: false
+    })
+  }
+
+  render () {
+
+    const posts = [
+    {
+      title: 'Dia',
+      description: 'O dia está ensolarado'
+    },
+    {
+      title: 'Noite',
+      description: 'A noite está estrelada'
+    }
+  ]
+
+    return (
+      <div>
+        <button onClick={this.onRemove}>Remover component</button>
+        {this.state.actived &&
+          <Twitter posts={posts} loading={this.state.loading}/>
+        }    
+      </div>
+      
+    )
+  }
+}
+
+export default App
