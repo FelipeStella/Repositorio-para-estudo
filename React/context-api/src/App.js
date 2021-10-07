c122cfe44f4988200e8248429ea1348c13115378
FelipeStella <felipedasilva.stella@gmail.com>
2021-10-07 20:51:49 -0300

    example-context-api

---
+++ React/context-api/src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import { ThemeContext, themes } from './Theme'
+import Card from './Card'
+
+function App () {
+
+  const [token, setToken] = useState()
+
+  useEffect(() => {
+    setTimeout(() => {
+      setToken('34q4m4k3ak43ak4kak4')
+    }, 4000)
+  }, [setToken])
+
+  return (
+    <ThemeContext.Provider value={{ ...themes.primary, token }}>
+      <Card />
+    </ThemeContext.Provider>
+  )
+  
+}
+
+export default App
